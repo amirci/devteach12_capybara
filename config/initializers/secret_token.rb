108bf72bc5bb132b358424e4bd90340bffee5e58
Amir Barylko <amir@barylko.com>
2012-02-23 14:04:44 -0600

    Base install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectPlan::Application.config.secret_token = '47fafa72e66de8faceac591ab243ed9290e555c074b6d7a0952a6c072c2af76a046713d8db2cb8bf44b0168fe76fcab2a1caa848694b23bf598de0e76655bca0'
