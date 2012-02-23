737ebb5d0d50b15ba30b24120e792246acdb73fc
Amir Barylko <amir@barylko.com>
2012-02-23 15:08:41 -0600

    Added project, now failing because I have no action

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,4 @@
+class ProjectsController < ApplicationController
+  
+  
+end
--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -1,9 +1,9 @@
 Given /^I have some projects loaded$/ do
-  10.times { |i| Project.create(mame: "Project #{i}", description: "The best project ever")}
+  10.times { |i| Project.create(name: "Project #{i}", description: "The best project ever")}
 end
 
 When /^I go to the projects page$/ do
-  pending # express the regexp above with the code you wish you had
+  visit('/projects')
 end
 
 Then /^I should the complete list of projects$/ do
