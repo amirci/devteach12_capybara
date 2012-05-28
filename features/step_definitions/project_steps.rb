33ac48fc043f1a757a2d422b9be18096a129795c
Amir Barylko <amir@barylko.com>
2012-05-28 16:37:36 -0700

    Implemented verifying the listing matches

--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -15,7 +15,11 @@ When /^I activate the project$/ do
 end
 
 Then /^I should see the complete list of projects$/ do
-  pending
+  actual = all(:css, "#projects tbody tr")
+    .map { |tr| tr.all("td").map(&:text) }
+    .map { |cells| {name: cells[0], description: cells[1], active: cells[2] == 'true'}}
+  expected = Project.all.map { |p| {name: p.name, description: p.description, active: p.active} }
+  actual.should == expected
 end
 
 Then /^the project should be shown active in the listing$/ do
